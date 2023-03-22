de0571fc0f8f1a168c9640d4e5af9854856cda4c
zachhudy <zachhudy@gmail.com>
2023-03-21 21:57:42 -0400

    Center grid; decrease size as well

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
         const newSquare = document.createElement('div');
         newSquare.classList.add('square');
-        newSquare.style.cssText = `height: ${960 / 16}px; width: ${960 / 16}px;`
+        newSquare.style.cssText = `height: ${640 / 16}px; width: ${640 / 16}px;`
         newRow.appendChild(newSquare);
     }
     container.appendChild(newRow);
