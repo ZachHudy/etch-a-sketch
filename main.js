22a4843391fc33455a983b2ea8e6cba934f100ff
zachhudy <zachhudy@gmail.com>
2023-03-23 10:28:23 -0400

    Add some comments to code; remove console logs, as were only for seeing events fired

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
+// select container of squares
 const container = document.querySelector('.container');
+// select entire body of page
 const body = document.querySelector('body');
+// set boolean for dragging depending on mousedown/mouseup
 let drawActive = false;
+// set default number of square grid (rows and columns both equal n)
 let n = 16;
+// toggle drawActive true status when mouse is depressed inside body
 body.addEventListener('mousedown', (e) => {
     drawActive = true;
     e.preventDefault();
-    console.log(drawActive);
-
 });
+// toggle drawActive false when mouse is released anywhere on screen
 window.addEventListener('mouseup', () => {
     drawActive = false;
-    console.log(drawActive);
 });
 
+// create function to create the grid; accepts n as an argument, which will decide number of rows and columns
 function createGrid(n) {
     for (let i = 0; i < n; i++) {
         const newRow = document.createElement('div');
